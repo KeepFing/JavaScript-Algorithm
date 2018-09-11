e89ccb3e3ddde984b078aee8830fb3e22d6f3717
KeepFing <bear393048@163.com>
2018-09-11 15:37:59 +0800

    十大排序算法

---
+++ Array/sort.js
@@ -0,0 +1,356 @@
+/*
+ * 数组的十大排序算法
+ * 
+ * */
+
+/*
+ * 一、冒泡排序（Bubble Sort）
+ * 1.比较相邻的两个元素，如果前一个比后一个大，则交换位置。
+ * 2.第一轮的时候最后一个元素应该是最大的一个。
+ * 3.按照步骤一的方法进行相邻两个元素的比较，这个时候最后一个元素已经是最大的了。
+ * 
+ * */
+let bubbleSort = (arr) => {
+	if(arr.length <= 1) {
+		return arr
+	}
+	for(var i = 0; i < arr.length - 1; i++) {
+		for(var j = 0; j < arr.length - i - 1; j++) {
+			if(arr[j] > arr[j + 1]) {
+				var swap = arr[j]
+				arr[j] = arr[j + 1]
+				arr[j + 1] = swap
+			}
+		}
+	}
+	return arr
+}
+
+/*
+ * 二、选择排序（Selection Sort）
+ * 
+ * 首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置，然后，再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。以此类推，直到所有元素均排序完毕。 
+ * 在时间复杂度上表现最稳定的排序算法之一，因为无论什么数据进去都是O(n²)的时间复杂度，所以用到它的时候，数据规模越小越好。唯一的好处可能就是不占用额外的内存空间。
+ * 
+ * */
+
+let selectionSort = (arr) => {
+    var len = arr.length;
+    var minIndex, temp;
+    for (var i = 0; i < len - 1; i++) {
+        minIndex = i;
+        for (var j = i + 1; j < len; j++) {
+            if (arr[j] < arr[minIndex]) {     //寻找最小的数
+                minIndex = j;                 //将最小数的索引保存
+            }
+        }
+        temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+    return arr;
+}
+
+/*
+ * 三、插入排序（Insertion Sort）
+ * 
+ * 1.从第一个元素开始，该元素可以认为已经被排序
+ * 2.取出下一个元素，在已经排序的元素序列中从后向前扫描
+ * 3.如果该元素（已排序）大于新元素，将该元素移到下一位置
+ * 4.重复步骤3，直到找到已排序的元素小于或者等于新元素的位置
+ * 5.将新元素插入到下一位置中
+ * 6.重复步骤2
+ * 
+ * */
+
+let insertionSort = (arr) => {
+    var len = arr.length;
+    var preIndex, current;
+    for (var i = 1; i < len; i++) {
+        preIndex = i - 1;
+        current = arr[i];
+        while(preIndex >= 0 && arr[preIndex] > current) {
+            arr[preIndex+1] = arr[preIndex];
+            preIndex--;
+        }
+        arr[preIndex+1] = current;
+    }
+    return arr;
+}
+
+
+/*
+ * 四、希尔排序（Shell Sort）
+ * 
+ * 先将整个待排序的记录序列分割成为若干子序列分别进行直接插入排序。 
+ * 希尔排序是插入排序的一种更高效率的实现。它与插入排序的不同之处在于，它会优先比较距离较远的元素。
+ * 希尔排序的核心在于间隔序列的设定。
+ * 既可以提前设定好间隔序列，也可以动态的定义间隔序列。
+ * 
+ * */
+
+let shellSort = (arr) => {
+    var len = arr.length,
+        temp,
+        gap = 1;
+    while(gap < len/3) {          //动态定义间隔序列
+        gap =gap*3+1;
+    }
+    for (gap; gap> 0; gap = Math.floor(gap/3)) {
+        for (var i = gap; i < len; i++) {
+            temp = arr[i];
+            for (var j = i-gap; j > 0 && arr[j]> temp; j-=gap) {
+                arr[j+gap] = arr[j];
+            }
+            arr[j+gap] = temp;
+        }
+    }
+    return arr;
+}
+
+/*
+ * 五、归并排序（Merge Sort) 
+ * 
+ * 归并排序是一种稳定的排序方法。将已有序的子序列合并，得到完全有序的序列；即先使每个子序列有序，再使子序列段间有序。
+ * 
+ * */
+
+let mergeSort = (arr) => {  //采用自上而下的递归方法
+    var len = arr.length;
+    if(len < 2) {
+        return arr;
+    }
+    var middle = Math.floor(len / 2),
+        left = arr.slice(0, middle),
+        right = arr.slice(middle);
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+let merge = (left, right) => {
+    var result = [];
+
+    while (left.length>0 && right.length>0) {
+        if (left[0] <= right[0]) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+
+    while (left.length)
+        result.push(left.shift());
+
+    while (right.length)
+        result.push(right.shift());
+
+    return result;
+}
+
+/*
+ * 六、快速排序（Quick Sort）
+ * 
+ * 快速排序是对冒泡排序的一种改进，第一趟排序时将数据分成两部分，一部分比另一部分的所有数据都要小。然后递归调用，在两边都实行快速排序。 
+ * 快速排序的内循环比大多数排序算法都要短小，这意味着它无论是在理论上还是在实际中都要更快。它的主要缺点是非常脆弱，在实现时要非常小心才能避免低劣的性能。
+ * 
+ * */
+
+let quickSort = (arr, left, right) => {
+    var len = arr.length,
+        partitionIndex,
+        left = typeof left != 'number' ? 0 : left,
+        right = typeof right != 'number' ? len - 1 : right;
+
+    if (left < right) {
+        partitionIndex = partition(arr, left, right);
+        quickSort(arr, left, partitionIndex-1);
+        quickSort(arr, partitionIndex+1, right);
+    }
+    return arr;
+}
+
+let partition = (arr, left ,right) => {     //分区操作
+    var pivot = left,                      //设定基准值（pivot）
+        index = pivot + 1;
+    for (var i = index; i <= right; i++) {
+        if (arr[i] < arr[pivot]) {
+            swap(arr, i, index);
+            index++;
+        }        
+    }
+    swap(arr, pivot, index - 1);
+    return index-1;
+}
+
+let swap = (arr, i, j) => {
+    var temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+/*
+ * 七、堆排序（Heap Sort）
+ * 
+ * 堆排序（Heapsort）是指利用堆这种数据结构所设计的一种排序算法。
+ * 堆积是一个近似完全二叉树的结构，并同时满足堆积的性质：即子结点的键值或索引总是小于（或者大于）它的父节点。
+ * 
+ * */
+
+var len;    //因为声明的多个函数都需要数据长度，所以把len设置成为全局变量
+
+let buildMaxHeap = (arr) => {   //建立大顶堆
+    len = arr.length;
+    for (var i = Math.floor(len/2); i &gt;= 0; i--) {
+        heapify(arr, i);
+    }
+}
+
+let heapify = (arr, i) => {     //堆调整
+    var left = 2 * i + 1,
+        right = 2 * i + 2,
+        largest = i;
+
+    if (left < len && arr[left] > arr[largest]) {
+        largest = left;
+    }
+
+    if (right < len && arr[right] > arr[largest]) {
+        largest = right;
+    }
+
+    if (largest != i) {
+        swap(arr, i, largest);
+        heapify(arr, largest);
+    }
+}
+
+let swap = (arr, i, j) => {
+    var temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+let heapSort = (arr) => {
+    buildMaxHeap(arr);
+
+    for (var i = arr.length-1; i > 0; i--) {
+        swap(arr, 0, i);
+        len--;
+        heapify(arr, 0);
+    }
+    return arr;
+}
+
+/*
+ * 八、计数排序（Counting Sort）
+ * 
+ * 计数排序使用一个额外的数组C，其中第i个元素是待排序数组A中值等于i的元素的个数。
+ * 然后根据数组C来将A中的元素排到正确的位置。它只能对整数进行排序。
+ * 
+ * */
+
+let countingSort = (arr, maxValue) => {
+    var bucket = new Array(maxValue+1),
+        sortedIndex = 0;
+        arrLen = arr.length,
+        bucketLen = maxValue + 1;
+
+    for (var i = 0; i < arrLen; i++) {
+        if (!bucket[arr[i]]) {
+            bucket[arr[i]] = 0;
+        }
+        bucket[arr[i]]++;
+    }
+
+    for (var j = 0; j < bucketLen; j++) {
+        while(bucket[j] > 0) {
+            arr[sortedIndex++] = j;
+            bucket[j]--;
+        }
+    }
+
+    return arr;
+}
+
+/*
+ * 九、 桶排序（Bucket Sort）
+ * 
+ * 假设输入数据服从均匀分布，将数据分到有限数量的桶里，每个桶再分别排序（有可能再使用别的排序算法或是以递归方式继续使用桶排序进行排序)
+ * 桶排序是计数排序的升级版。它利用了函数的映射关系，高效与否的关键就在于这个映射函数的确定。
+ * 
+ * */
+
+let bucketSort = (arr, bucketSize) => {
+    if (arr.length === 0) {
+      return arr;
+    }
+
+    var i;
+    var minValue = arr[0];
+    var maxValue = arr[0];
+    for (i = 1; i < arr.length; i++) {
+      if (arr[i] < minValue) {
+          minValue = arr[i];                //输入数据的最小值
+      } else if (arr[i] > maxValue) {
+          maxValue = arr[i];                //输入数据的最大值
+      }
+    }
+
+    //桶的初始化
+    var DEFAULT_BUCKET_SIZE = 5;            //设置桶的默认数量为5
+    bucketSize = bucketSize || DEFAULT_BUCKET_SIZE;
+    var bucketCount = Math.floor((maxValue - minValue) / bucketSize) + 1;   
+    var buckets = new Array(bucketCount);
+    for (i = 0; i < buckets.length; i++) {
+        buckets[i] = [];
+    }
+
+    //利用映射函数将数据分配到各个桶中
+    for (i = 0; i < arr.length; i++) {
+        buckets[Math.floor((arr[i] - minValue) / bucketSize)].push(arr[i]);
+    }
+
+    arr.length = 0;
+    for (i = 0; i < buckets.length; i++) {
+        insertionSort(buckets[i]);                      //对每个桶进行排序，这里使用了插入排序
+        for (var j = 0; j < buckets[i].length; j++) {
+            arr.push(buckets[i][j]);                      
+        }
+    }
+
+    return arr;
+}
+
+/* 
+ * 十、基数排序（Radix Sort）
+ * 
+ * 基数排序是按照低位先排序，然后收集；再按照高位排序，然后再收集；依次类推，直到最高位。
+ * 有时候有些属性是有优先级顺序的，先按低优先级排序，再按高优先级排序。
+ * 最后的次序就是高优先级高的在前，高优先级相同的低优先级高的在前。
+ * 基数排序基于分别排序，分别收集，所以是稳定的。
+ * 
+ * */
+
+var counter = [];
+let radixSort = (arr, maxDigit) => {
+    var mod = 10;
+    var dev = 1;
+    for (var i = 0; i < maxDigit; i++, dev *= 10, mod *= 10) {
+        for(var j = 0; j < arr.length; j++) {
+            var bucket = parseInt((arr[j] % mod) / dev);
+            if(counter[bucket]==null) {
+                counter[bucket] = [];
+            }
+            counter[bucket].push(arr[j]);
+        }
+        var pos = 0;
+        for(var j = 0; j < counter.length; j++) {
+            var value = null;
+            if(counter[j]!=null) {
+                while ((value = counter[j].shift()) != null) {
+                      arr[pos++] = value;
+                }
+          }
+        }
+    }
+    return arr;
+}
