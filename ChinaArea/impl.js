2e818a57956955dc65ea01d01659ef7a45d2914d
KeepFing <bear393048@163.com>
2018-10-23 14:48:01 +0800

    省市区联动

--- ChinaArea/impl.js
+++ ChinaArea/impl.js
@@ -1,6 +1,9 @@
-let area = require('./area.js').area
+let area = require('./area.js').area	//引入省市区的json格式的文件
 
-
+/**
+ * 传入选择的省，返回该省下的市列表
+ * 
+ * **/
 let selectProvice = (provice)=>{
 	for (let v of area) {
 		if (v.name == provice) {
@@ -9,6 +12,13 @@ let selectProvice = (provice)=>{
 	}
 }
 
+/**
+ * 传入省、市
+ * 返回该省、市下的县(区)列表
+ * 
+ * **/
+
+
 let selectCity = (provice,city)=>{
 	for (let v of selectProvice(provice)) {
 		if (v.name == city) {
@@ -16,5 +26,3 @@ let selectCity = (provice,city)=>{
 		}
 	}
 }
-
-console.log(selectCity('四川省','巴中市'))
